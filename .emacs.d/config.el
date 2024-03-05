6b99029559a437a9950f6f95eee5658defe841bb
whydouneedmynick <whydouneedmynick@gmail.com>
2024-03-05 07:40:02 +0100

    add(emacs): Vterm autocompletion

--- .emacs.d/config.el
+++ .emacs.d/config.el
@@ -8,7 +8,7 @@
   :init
   (gcmh-mode 1))
 
-(setq-default debug-on-error t)
+(setq-default debug-on-error nil)
 
 (require 'pyright-write)
 (require 'suzu-buffer)
@@ -659,7 +659,39 @@
   :config
   (setq vterm-shell "/usr/bin/bash"
         vterm-buffer-name-string "vterm %s"
-        vterm-max-scrollback 5000))
+        vterm-max-scrollback 5000)
+  (defun get-full-list ()
+    (let ((program-list (process-lines "bash" "-c" "compgen -c"))
+          (file-directory-list (process-lines "bash" "-c" "compgen -f"))
+          (history-list (with-temp-buffer
+                          (insert-file-contents "~/.bash_history")
+                          (split-string (buffer-string) "\n" t))))
+
+      (delete-dups (append program-list file-directory-list history-list))))
+
+  (defun vterm-completion-choose-item ()
+    (completing-read "Choose: " (get-full-list) nil nil (thing-at-point 'word 'no-properties)))
+
+  (defun vterm-completion ()
+    (interactive)
+    (vterm-directory-sync)
+    (setq vterm-chosen-item (vterm-completion-choose-item))
+    (when (thing-at-point 'word)
+      (vterm-send-meta-backspace))
+    (vterm-send-string vterm-chosen-item))
+
+  (defun vterm-directory-sync ()
+    "Synchronize current working directory."
+    (interactive)
+    (when vterm--process
+      (let* ((pid (process-id vterm--process))
+             (dir (file-truename (format "/proc/%d/cwd/" pid))))
+        (setq default-directory dir))))
+
+  :general
+  (:states 'insert
+           :keymaps 'vterm-mode-map
+           "<tab>" 'vterm-completion))
 
 (use-package vterm-toggle
   :ensure t
