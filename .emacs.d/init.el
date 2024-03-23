695fa8cb3e3a623b83f7077dd265d7736a544885
whydouneedmynick <whydouneedmynick@gmail.com>
2024-03-23 06:45:23 +0100

    add(emacs): A lot of cool stuff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,4 +13,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auth-source-save-behavior nil))
+ '(auth-source-save-behavior nil)
+ '(package-selected-packages
+   '(helpful alert bufler perspective which-key tldr sudo-edit multi-vterm vterm-toggle vterm pcmpl-args eshell-toggle eshell-git-prompt eshell-syntax-highlighting rainbow-mode consult vertico all-the-icons-completion marginalia pdf-tools org-roam org-auto-tangle org-appear org-bullets toc-org visual-fill-column emojify dap-mode csv-mode markdown-mode sqlformat yuck-mode python-black rust-mode eldoc-box dashboard evil-nerd-commenter elfeed-goodies elfeed peep-dired dired-open diminish nerd-icons-corfu orderless corfu all-the-icons-dired all-the-icons git-gutter magit zen-mode golden-ratio general undo-tree gcmh evil-collection better-jumper async)))
