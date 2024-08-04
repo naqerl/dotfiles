df4ce8847fffc5cafe1fe0cb250b5a2b685dc6ef
suzumenobu <idevtier@gmail.com>
2024-08-04 13:13:07 +0700

    updte(bin & hyprland): Move to org config of bin files

--- .config/bin/music-torrent-downloaded.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo "$1" > ~/music-torrent-downloaded
--- .config/hypr/startup.sh
+++ .config/hypr/startup.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/env bash
 hyprctl dispatch exec $HOME/.config/bin/notifications
 hyprctl dispatch exec [workspace 1 silent] qutebrowser
-# hyprctl dispatch exec [workspace 2 silent] 'kitty bash -c "tmux new -s main || tmux a"'
+hyprctl dispatch exec emacs --daemon && notify-send "Emacs started"
 hyprctl dispatch workspace 2
