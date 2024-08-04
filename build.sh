d806176d1ffc317293fd60552ab0a85e79b84a36
suzumenobu <idevtier@gmail.com>
2024-08-04 13:13:07 +0700

    fix(build): Rust install

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@ set -e
 
 git pull origin main
 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
+rustup default stable
+
 if [[ -z $(command -v paru) ]]; then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-    source "$HOME/.cargo/env"
     sudo pacman -S --needed base-devel
     git clone https://aur.archlinux.org/paru.git
     cd paru
