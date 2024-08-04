f3bcbb9fc002f6506d416887d8d24fd98154c326
suzumenobu <idevtier@gmail.com>
2024-08-04 13:13:08 +0700

    fix(emacs): Elisp formatting, typescript mode, restclient & minor updates

--- .emacs.d/scripts/suzu-buffer.el
+++ .emacs.d/scripts/suzu-buffer.el
@@ -4,19 +4,23 @@
   "Switch to previous buffer in the current perspective"
   (interactive)
   (let* ((buffers (persp-current-buffer-names t))
-	 (prev-buffer-idx (- (cl-position (buffer-name) buffers) 1))
-	 (prev-buffer-name (if (< prev-buffer-idx 0) (car (last buffers)) (nth prev-buffer-idx buffers))))
-    (persp-switch-to-buffer prev-buffer-name)
-    ))
+         (prev-buffer-idx (- (cl-position (buffer-name) buffers) 1))
+         (prev-buffer-name
+          (if (< prev-buffer-idx 0)
+              (car (last buffers))
+            (nth prev-buffer-idx buffers))))
+    (persp-switch-to-buffer prev-buffer-name)))
 
 (defun suzu/buffer-next ()
   "Switch to next buffer in the current perspective"
   (interactive)
   (let* ((buffers (persp-current-buffer-names t))
-	 (next-buffer-idx (+ (cl-position (buffer-name) buffers) 1))
-	 (next-buffer-name (if (>= next-buffer-idx (safe-length buffers)) (nth 0 buffers) (nth next-buffer-idx buffers))))
-    (persp-switch-to-buffer next-buffer-name)
-    ))
+         (next-buffer-idx (+ (cl-position (buffer-name) buffers) 1))
+         (next-buffer-name
+          (if (>= next-buffer-idx (safe-length buffers))
+              (nth 0 buffers)
+            (nth next-buffer-idx buffers))))
+    (persp-switch-to-buffer next-buffer-name)))
 
 (defun copy-full-path-to-kill-ring ()
   "copy buffer's full path to kill ring"
@@ -24,11 +28,13 @@
   (when buffer-file-name
     (let ((path (file-truename buffer-file-name)))
       (kill-new (file-truename buffer-file-name))
-      (message "Path copied [%s]" path)
-      )
-    ))
+      (message "Path copied [%s]" path))))
+
+(defun suzu/kill-current-buffer ()
+  "Kill current buffer."
+  (interactive)
+  (kill-buffer (current-buffer)))
 
 (defun suzu/center-buffer ()
   (let ((margin-size (/ (- (frame-width) 80) 2)))
     (set-window-margins nil margin-size margin-size)))
-
--- .emacs.d/scripts/suzu-project.el
+++ .emacs.d/scripts/suzu-project.el
@@ -1,22 +1,53 @@
 (require 'project)
+(require 'suzu-extensions)
 
-(defun suzu/get-last-two-elements (dir)
+(defun suzu/project--get-last-two-elements (dir)
   "Get the last two elements of a path."
   (let* ((dir-components (split-string dir "\/" t))
          (last-two (last dir-components 2))
-         (result (if (string-match-p "\\(~\\|suzu\\).*" (car last-two)) (last last-two 1) last-two)))
+         (result
+          (if (string-match-p "\\(~\\|suzu\\).*" (car last-two))
+              (last last-two 1)
+            last-two)))
     (mapconcat 'identity result "/")))
 
-(defun suzu/project-name-function (project-root)
-  (suzu/get-last-two-elements project-root))
-
 (defun suzu/project-switch-in-new-perspective ()
+  "Opens project in a new perspective"
   (interactive)
   (let* ((project-dir (project-prompt-project-dir))
-         (persp-name (suzu/project-name-function project-dir)))
+         (persp-name
+          (suzu/project--get-last-two-elements project-dir)))
     (persp-switch persp-name)
     (message project-dir)
-    (project-switch-project project-dir)
-    ))
+    (project-switch-project project-dir)))
+
+(defun suzu/project-discover-in-directory (directory &optional depth)
+  "Recursively searches projects under given directroy.
+   Default depth is 4
+   Returns number of total found projects"
+  (interactive (list (read-directory-name "Base search path: ")))
+  (or depth (setq depth 4))
+
+  (when (not (file-directory-p directory))
+    (error "Base path should be a directory."))
+
+  (message "Searching projects in %s" directory)
+
+  (let ((projects-found (project-remember-projects-under directory)))
+    (when (= projects-found 0)
+      (if (and (numberp depth) (> depth 0))
+          (dolist (dir
+                   (ignore-errors
+                     (directory-files directory
+                                      t
+                                      "^\\([^.]\\|\\.\\..\\)")))
+            (when (file-directory-p dir)
+              (setq projects-found
+                    (+ projects-found
+                       (suzu/project-discover-in-directory
+                        dir
+                        (1- depth))))))))
+    (message "Total projects found: %s" projects-found)
+    projects-found))
 
 (provide 'suzu-project)
