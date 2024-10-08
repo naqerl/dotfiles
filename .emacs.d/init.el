a3460d0037c5a3f34dc9c1963506fbd033e75869
suzumenobu <idevtier@gmail.com>
2024-10-09 05:40:40 +0800

    add: Emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,92 @@
+(org-babel-load-file
+ (expand-file-name "config.org" user-emacs-directory))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-hide ((t nil))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
+ '(enwc-wired-device "wlan0")
+ '(enwc-wireless-device "wlan0")
+ '(org-agenda-files nil)
+ '(package-selected-packages
+   '(python-isort
+     jtsx
+     prettier-js
+     web-mode
+     treesit-auto
+     poetry
+     tree-sitter-langs
+     doom-modeline
+     cape
+     indent-guide
+     docker-compose-mode
+     mu4e
+     typescript-mode
+     elisp-autofmt
+     eshell-syntax-highlighting
+     enwc
+     emacs-nm
+     org-bullets
+     helpful
+     alert
+     bufler
+     perspective
+     which-key
+     tldr
+     sudo-edit
+     multi-vterm
+     vterm-toggle
+     vterm
+     pcmpl-args
+     eshell-toggle
+     eshell-git-prompt
+     rainbow-mode
+     consult
+     vertico
+     all-the-icons-completion
+     marginalia
+     pdf-tools
+     org-roam
+     org-auto-tangle
+     org-appear
+     toc-org
+     visual-fill-column
+     emojify
+     dap-mode
+     csv-mode
+     markdown-mode
+     sqlformat
+     yuck-mode
+     python-black
+     rust-mode
+     eldoc-box
+     dashboard
+     evil-nerd-commenter
+     elfeed-goodies
+     elfeed
+     peep-dired
+     dired-open
+     diminish
+     nerd-icons-corfu
+     orderless
+     corfu
+     all-the-icons-dired
+     all-the-icons
+     git-gutter
+     magit
+     zen-mode
+     golden-ratio
+     general
+     undo-tree
+     gcmh
+     evil-collection
+     better-jumper
+     async))
+ '(warning-suppress-types '((comp))))
---
+++ .emacs.d/scripts/suzu-buffer.el
@@ -0,0 +1,40 @@
+(provide 'suzu-buffer)
+
+(defun suzu/buffer-prev ()
+  "Switch to previous buffer in the current perspective"
+  (interactive)
+  (let* ((buffers (persp-current-buffer-names t))
+         (prev-buffer-idx (- (cl-position (buffer-name) buffers) 1))
+         (prev-buffer-name
+          (if (< prev-buffer-idx 0)
+              (car (last buffers))
+            (nth prev-buffer-idx buffers))))
+    (persp-switch-to-buffer prev-buffer-name)))
+
+(defun suzu/buffer-next ()
+  "Switch to next buffer in the current perspective"
+  (interactive)
+  (let* ((buffers (persp-current-buffer-names t))
+         (next-buffer-idx (+ (cl-position (buffer-name) buffers) 1))
+         (next-buffer-name
+          (if (>= next-buffer-idx (safe-length buffers))
+              (nth 0 buffers)
+            (nth next-buffer-idx buffers))))
+    (persp-switch-to-buffer next-buffer-name)))
+
+(defun copy-full-path-to-kill-ring ()
+  "copy buffer's full path to kill ring"
+  (interactive)
+  (when buffer-file-name
+    (let ((path (file-truename buffer-file-name)))
+      (kill-new (file-truename buffer-file-name))
+      (message "Path copied [%s]" path))))
+
+(defun suzu/kill-current-buffer ()
+  "Kill current buffer."
+  (interactive)
+  (kill-buffer (current-buffer)))
+
+(defun suzu/center-buffer ()
+  (let ((margin-size (/ (- (frame-width) 80) 2)))
+    (set-window-margins nil margin-size margin-size)))
