4c6d05ba896c02459d10f211426549abb030e91f
Andrey <idevtier@gmail.com>
2024-11-29 08:13:32 +0100

    fix: Update emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,68 +17,18 @@
  '(enwc-wireless-device "wlan0")
  '(org-agenda-files nil)
  '(package-selected-packages
-   '(alert
-     all-the-icons-completion
-     all-the-icons-dired
-     bufler
-     consult
-     corfu
-     csv-mode
-     dashboard
-     dimmer
-     dired-open
-     dockerfile-mode
-     drag-stuff
-     eldoc-box
-     elf-mode
-     elfeed-goodies
-     elisp-autofmt
-     emojify
-     enwc
-     eshell-git-prompt
-     eshell-syntax-highlighting
-     eshell-toggle
-     evil-nerd-commenter
-     flycheck-mypy
-     gcmh
-     general
-     git-gutter
-     go-mode
-     golden-ratio
-     helpful
-     indent-guide
-     json-mode
-     jtsx
-     load-env-vars
-     magit
-     marginalia
-     markdown-mode
-     nerd-icons-corfu
-     nushell-mode
-     nushell-ts-mode
-     orderless
-     org-auto-tangle
-     org-present
-     pcmpl-args
-     peep-dired
-     perspective
-     php-mode
-     plantuml-mode
-     poetry
-     prettier-js
-     python-isort
-     rainbow-mode
-     ruff-format
-     rust-mode
-     sqlformat
-     sudo-edit
-     tldr
-     toc-org
-     typescript-mode
-     undo-tree
-     vertico
-     vterm-toggle
-     web-mode
-     yaml-mode
-     yuck-mode))
+   '(alert all-the-icons-completion all-the-icons-dired bufler consult
+           corfu csv-mode dashboard dimmer dired-open dockerfile-mode
+           drag-stuff eldoc-box elf-mode elfeed-goodies elisp-autofmt
+           elysium emojify enwc eshell-git-prompt
+           eshell-syntax-highlighting eshell-toggle
+           evil-nerd-commenter flycheck-mypy gcmh general git-gutter
+           go-mode golden-ratio gptel helpful indent-guide json-mode
+           jtsx load-env-vars magit marginalia markdown-mode
+           nerd-icons-corfu nushell-mode nushell-ts-mode orderless
+           org-auto-tangle org-present pcmpl-args peep-dired
+           perspective php-mode plantuml-mode poetry prettier-js
+           python-isort rainbow-mode ruff-format rust-mode sqlformat
+           sudo-edit tldr toc-org typescript-mode undo-tree vertico
+           vterm-toggle web-mode yaml-mode yuck-mode))
  '(warning-suppress-types '((comp))))
