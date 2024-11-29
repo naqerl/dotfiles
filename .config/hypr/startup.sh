7987a03a9640acf8a4a563c1fafe9de5c1874673
Andrey <idevtier@gmail.com>
2024-11-29 08:12:59 +0100

    fix: Update hyprland org config

--- .config/hypr/startup.sh
+++ .config/hypr/startup.sh
@@ -1,4 +1,26 @@
-hyprctl dispatch exec $HOME/.config/bin/notifications
-hyprctl dispatch exec [workspace 1 silent] qutebrowser
-hyprctl dispatch exec emacs --daemon && notify-send "Emacs started"
-hyprctl dispatch workspace 2
+#!/usr/bin/env bash
+
+set_value() {
+  key="$1"
+  value="$2"
+  eval "assoc_$key='$value'"
+}
+
+get_value() {
+  key="$1"
+  eval "echo \${assoc_$key}"
+}
+
+handle() {
+  case $1 in
+    openwindow*) echo "New window $1" ;;
+  esac
+}
+
+set_value 'emacs' '1'
+set_value 'zen-alpha' '2'
+
+socat -U - UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r line; do handle "$line"; done
+
+# hyprctl dispatch exec 'emacsclient --create-frame --alternate-editor=""'
+# hyprctl dispatch exec  'zen-browser'
