8bcd845b2a9b8c9dd80e272303a5c58f4783d072
Andrey <idevtier@gmail.com>
2024-12-19 10:41:18 +0100

    add: Project new from shell

---
+++ .emacs.d/scripts/project-new.el
@@ -0,0 +1,64 @@
+;;;; project-new --- Handy ways to create new project ;; -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; Create new projects with ease
+
+(require 'my-extensions)
+(require 'project)
+
+(defvar project-new-created-hook nil
+  "Hook that runs after project creation.
+PROJECT-PATH provided as argument")
+
+(defvar project-new--git-clone-from nil
+  "Source to clone from.")
+
+(defvar project-new--name nil
+  "Name for the new project.")
+
+(defconst project-git-base-path (expand-file-name "~/code/")
+  "Base path to clone to.")
+
+;;; Code:
+(defun project-new-git-clone ()
+  "Clones project to the given directory."
+  (interactive)
+  (let* ((default-directory
+          (read-directory-name "Base directory: "
+                               project-git-base-path))
+         (project-new--git-clone-from (read-string "Clone from: "))
+         (project-dir-name
+          (car
+           (my/track-new-directories
+            '(shell-command
+              (format "git clone %s" project-new--git-clone-from)))))
+         (project-path
+          (expand-file-name (format "%s/" project-dir-name))))
+    (project-new--after-created project-path)))
+
+(defun project-new-cl-project ()
+  "Creates new project with ~ros init~."
+  (interactive)
+  (let* ((default-directory
+          (read-directory-name "Base directory: "
+                               project-git-base-path))
+         (project-new--name (read-string "Project name: "))
+         (project-path
+          (expand-file-name (format "%s/" project-new--name))))
+    (with-temp-buffer
+      (shell-command (format
+                      "mkdir %s; cd %s; git init; make-project %s"
+                      project-new--name
+                      project-new--name
+                      project-new--name)
+                     t t))
+    (project-new--after-created project-path)))
+
+(defun project-new--after-created (project-path)
+  "Call after project created and pass PROJECT-PATH."
+  (message "After project created at %s" project-path)
+  (project-remember-projects-under project-path)
+  (run-hook-with-args 'project-new-created-hook project-path))
+
+(provide 'project-new)
+;;; project-new.el ends here
