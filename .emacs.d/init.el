87879ce1b80e9a99bdbc62c8a53e24d8b3cf9abf
Andrey <idevtier@gmail.com>
2025-01-04 20:33:00 +0100

    fix: Delete all custom faces from init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,18 +6,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("3bac409022f5772483b802a9d5b78155bf9f4256cc5dc556426fcd078709218c"
+     default))
  '(package-selected-packages '(git-gutter-fringe make-project nginx-mode rust-docs))
  '(package-vc-selected-packages '((rust-docs :url "https://github.com/scipunch/rust-docs")))
  '(warning-suppress-types '((comp))))
 (put 'downcase-region 'disabled nil)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(font-lock-doc-face ((t (:inherit italic :foreground "#868686"))))
- '(font-lock-keyword-face ((t (:inherit italic :foreground "#BEC3C6" :weight bold))))
- '(font-lock-string-face ((t (:inherit italic :foreground "#738678"))))
- '(fringe ((t (:foreground "#BEC3C6" :background "#BEC3C6"))))
- '(org-code ((t (:inherit default :background "black" :foreground "#e6e6e6" :box (:line-width (1 . 1) :color "#868686" :style released-button)))))
- '(org-table ((t (:inherit default :foreground "#b6b6b6")))))
