64059e9da2e134c6cdc6d2868ad5ad97308f06e9
Andrey <idevtier@gmail.com>
2025-01-04 10:07:29 +0100

    add: Improve ssh UX

--- .emacs.d/scripts/ssh.el
+++ .emacs.d/scripts/ssh.el
@@ -21,12 +21,30 @@
   (interactive)
   (unless (length= ssh-port-forward 3)
     (error "[ssh]: Wrong variable `ssh-port-forward' format"))
-  (async-shell-command
-   (apply #'format "ssh -v -N -L %s:localhost:%s %s" ssh-port-forward)
-   (get-buffer-create
-    (apply #'format "*ssh-%s:%s-%s*" ssh-port-forward))))
+  (let ((output-buffer-name
+         (apply #'format "*ssh-%s:%s-%s*" ssh-port-forward)))
+    (if (get-buffer output-buffer-name)
+        (warn
+         "[ssh]: Buffer %s already exists, a new process will not be created"
+         output-buffer-name)
+      (progn
+        (get-buffer-create output-buffer-name)
+        (async-shell-command (apply #'format
+                                    "ssh -v -N -L %s:localhost:%s %s"
+                                    ssh-port-forward)
+                             output-buffer-name)
+        (message "Starting port forwarding in %s"
+                 output-buffer-name)))))
 
 ; end-region   -- Port forwarding
+
+; begin-region -- UI Setup
+
+(add-to-list
+ 'display-buffer-alist '("\\*ssh-.*\\*" (display-buffer-no-window))
+ t (lambda (lhs rhs) (string= (car lhs) (car rhs))))
+
+; end-region   -- UI Setup
 
 (provide 'ssh)
 ;;; ssh.el ends here
