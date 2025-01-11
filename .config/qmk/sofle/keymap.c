6e4c36dc0257efd6a477130470357b7a881964b0
Andrey <idevtier@gmail.com>
2025-01-11 09:07:29 +0100

    fix: Actualize QMK Sofle keymap & improve gaming UX

--- .config/qmk/sofle/keymap.c
+++ .config/qmk/sofle/keymap.c
@@ -1,7 +1,6 @@
 #include QMK_KEYBOARD_H
 
 enum sofle_layers {
-  /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
   _QWERTY,
   _GAMING,
   _LOWER,
@@ -11,73 +10,65 @@ enum sofle_layers {
 
 enum custom_keycodes {
   KC_QWERTY = SAFE_RANGE,
-  KC_GAMING,
-  KC_LOWER,
-  KC_RAISE,
-  KC_ADJUST,
-  KC_PRVWD,
-  KC_NXTWD,
-  KC_LSTRT,
-  KC_LEND,
-  KC_DLINE,
-  KC_BSPC_DEL,
+  KC_GMG,
+  KC_LWR,
+  KC_RSE,
+  KC_ADJ,
+  KC_BSD, // SmartBackspaceDelete
   KC_LAYER
 };
 
+#define MKC_A    MT(MOD_LGUI, KC_A)
+#define MKC_S    MT(MOD_LALT, KC_S)
+#define MKC_D    MT(MOD_LSFT, KC_D)
+#define MKC_F    MT(MOD_LCTL, KC_F)
+#define MKC_J    MT(MOD_RCTL, KC_J)
+#define MKC_K    MT(MOD_RSFT, KC_K)
+#define MKC_L    MT(MOD_RALT, KC_L)
+#define MKC_SCLN MT(MOD_RGUI, KC_SCLN)
+
+// Keycodes description could be found in [[file:~/qmk_firmware/docs/keycodes.md::Keycodes Overview][keycodes.md]]
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
-		     _______,                       KC_F13,               KC_F14,    KC_F15,      KC_F20,    _______,                                                        KC_MEDIA_PREV_TRACK,    KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK,    KC_MEDIA_SELECT,    _______,  _______,
-		     KC_ESC,                       KC_Q,               KC_W,    KC_E,      KC_R,    KC_T,                                                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC_DEL,
-		     KC_TAB,   MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F),                 KC_G,    KC_H,    MT(MOD_RCTL, KC_J), MT(MOD_RSFT, KC_K), MT(MOD_LALT, KC_L), MT(MOD_RGUI, KC_SCLN),  KC_QUOT,
-		     KC_GRV,                      KC_Z,               KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,                                                       KC_GAMING, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_BSLS,
-                     _______,KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_LOWER, KC_SPC,                                                     KC_ENT,  KC_RAISE, KC_LPRN, KC_RPRN, KC_PRINT_SCREEN
-		     ),
+                     XXXXXXX, KC_F13,  KC_F14,   KC_F15,  KC_F20,   XXXXXXX,                      KC_MPRV, KC_MPLY, KC_MNXT, KC_MSEL, XXXXXXX,  XXXXXXX,
+                     KC_ESC,  KC_Q,    KC_W,     KC_E,    KC_R,     KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSD,
+                     KC_TAB,  MKC_A,   MKC_S,    MKC_D,   MKC_F,    KC_G,                         KC_H,    MKC_J,   MKC_K,   MKC_L,   MKC_SCLN, KC_QUOT,
+                     KC_GRV,  KC_Z,    KC_X,     KC_C,    KC_V,     KC_B,    KC_MUTE,    KC_GMG,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_BSLS,
+                                       XXXXXXX,  KC_LBRC, KC_RBRC,  KC_LWR,  KC_SPC,     KC_ENT,  KC_RSE,  KC_LPRN, KC_RPRN, KC_PSCR
+                     ),
 
 
   [_GAMING] = LAYOUT(
-		     KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC_DEL,
-		     KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_GRV,
-		     KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_UP,  KC_QUOT,
-		     KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MPLY,     KC_GAMING,KC_N,    KC_M, KC_COMM,  KC_LEFT, KC_DOWN,  KC_RIGHT,
-		     KC_LGUI,KC_LALT,KC_LAYER, KC_LCTL, KC_SPC,      KC_ENT,  KC_LBRC, KC_RBRC, KC_MINS, KC_EQL
+		     KC_LWR,  KC_1,    KC_2,     KC_3,    KC_4,     KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     _______,
+		     _______, _______, _______,  _______, _______,  _______,                      _______, _______, _______, _______, _______,  _______,
+		     _______, KC_A,    KC_S,     KC_D,    KC_F,     KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  _______,
+		     _______, _______, _______,  _______, _______,  _______, _______,    _______, _______, _______, _______, _______, _______,  _______,
+		                       KC_LGUI,  KC_LALT, KC_LSFT,  KC_LCTL, _______,    _______, _______, _______, _______, _______
 		     ),
 
   [_LOWER] = LAYOUT(
-		    _______,      _______,    _______,    _______,    _______,   _______,                       _______,   _______,   _______,   _______,  _______,  _______,
-		    _______,      KC_F1,      KC_F2,      KC_F3,      KC_F4, KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
-		    _______,      KC_1,       KC_2,       KC_3,       KC_4, KC_5,                       KC_6,   KC_7,   KC_8,   KC_9,  KC_0,  KC_F12,
-		    KC_CAPS_LOCK, XXXXXXX,    KC_MINUS,   KC_EQUAL,   KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, _______,       _______, KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_DEL, KC_END, KC_PGDN, XXXXXXX,
-                    CG_TOGG, KC_RALT, _______, KC_RCTL, _______,       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+		     _______, _______, _______,  _______,  _______, _______,                      KC_MPRV, KC_MPLY, KC_MNXT, KC_MSEL, _______,  _______,
+		     _______, KC_F1,   KC_F2,    KC_F3,    KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,
+		     _______, KC_1,    KC_2,     KC_3,     KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_F12,
+		     KC_CAPS, XXXXXXX, KC_MINUS, KC_EQUAL, _______, _______, _______,    _______, _______, _______, KC_DEL,  KC_END,  KC_PGDN,  XXXXXXX,
+                                       CG_TOGG,  KC_RALT,  _______, KC_RCTL, _______,    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 		    ),
 
   [_RAISE] = LAYOUT(
-		    QK_CLEAR_EEPROM, _______ , _______ , _______ , _______ , _______,                           QK_DYNAMIC_MACRO_RECORD_START_1,  QK_DYNAMIC_MACRO_RECORD_START_2  , QK_DYNAMIC_MACRO_RECORD_STOP,  _______ ,  _______ ,_______,
-		    _______,  KC_INS,  KC_PSCR,   KC_APP,  XXXXXXX, XXXXXXX,                        KC_PGUP, KC_PRVWD,   KC_UP, KC_NXTWD,KC_MEDIA_PLAY_PAUSE, KC_BSPC,
-		    _______, KC_LALT,  KC_LCTL,  KC_DEL,  XXXXXXX, KC_CAPS,                       KC_LEFT,  KC_DOWN, KC_UP, KC_RGHT,  KC_DEL, KC_BSPC,
-		    _______,KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, XXXXXXX,  _______,       _______,  XXXXXXX, KC_LSTRT, XXXXXXX, KC_LEND,   _______, _______,
-		    _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
+		     EE_CLR,   QK_RBT,  QK_BOOT, _______, _______, _______,                       DM_REC1, DM_REC2, DM_RSTP,  DM_PLY1, DM_PLY2, _______,
+		     _______,  KC_INS,  KC_PSCR, KC_APP,  XXXXXXX, XXXXXXX,                       KC_PGUP, _______, KC_UP,  _______,KC_MEDIA_PLAY_PAUSE, KC_BSPC,
+		     _______,  KC_LALT, KC_LCTL, KC_DEL,  XXXXXXX, KC_CAPS,                       KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,  KC_DEL, KC_BSPC,
+		     _______,  KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, XXXXXXX,  _______,    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   _______, _______,
+		                        _______, _______, _______, _______,  _______,    _______, _______, _______, _______, _______
 		    ),
-  /* ADJUST
-   * ,-----------------------------------------.                    ,-----------------------------------------.
-   * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
-   * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-   * | QK_BOOT|      |QWERTY|      |      |      |                    |      |      |      |      |      |      |
-   * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-   * |      |      |MACWIN|      |      |      |-------.    ,-------|      | VOLDO| MUTE | VOLUP|      |      |
-   * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
-   * |      |      |      |      |      |      |-------|    |-------|      | PREV | PLAY | NEXT |      |      |
-   * `-----------------------------------------/       /     \      \-----------------------------------------'
-   *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
-   *            |      |      |      |      |/       /         \      \ |      |      |      |      |
-   *            `----------------------------------'           '------''---------------------------'
-   */
+
   [_ADJUST] = LAYOUT(
-		     XXXXXXX , XXXXXXX,  XXXXXXX ,  XXXXXXX , XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-		     QK_BOOT, XXXXXXX,KC_QWERTY,XXXXXXX,CG_TOGG,XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-		     XXXXXXX , XXXXXXX,CG_TOGG, XXXXXXX,    QK_BOOT,  XXXXXXX,                     XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX,
-		     XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,  XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,
-		     _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
+		     XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+		     QK_BOOT,  XXXXXXX, XXXXXXX, XXXXXXX, CG_TOGG, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+		     XXXXXXX , XXXXXXX, CG_TOGG, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX,
+		     XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,
+		                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 		     )
 };
 // clang-format on
@@ -339,7 +330,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       set_single_persistent_default_layer(_QWERTY);
     }
     return false;
-  case KC_GAMING:
+  case KC_GMG:
     if (record->event.pressed) {
       if (get_highest_layer(default_layer_state) == _QWERTY) {
 	set_single_persistent_default_layer(_GAMING);
@@ -348,7 +339,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
     }
     return false;
-  case KC_LOWER:
+  case KC_LWR:
     if (record->event.pressed) {
       layer_on(_LOWER);
       update_tri_layer(_LOWER, _RAISE, _ADJUST);
@@ -357,7 +348,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       update_tri_layer(_LOWER, _RAISE, _ADJUST);
     }
     return false;
-  case KC_RAISE:
+  case KC_RSE:
     if (record->event.pressed) {
       layer_on(_RAISE);
       update_tri_layer(_LOWER, _RAISE, _ADJUST);
@@ -366,142 +357,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       update_tri_layer(_LOWER, _RAISE, _ADJUST);
     }
     return false;
-  case KC_ADJUST:
+  case KC_ADJ:
     if (record->event.pressed) {
       layer_on(_ADJUST);
     } else {
       layer_off(_ADJUST);
     }
     return false;
-  case KC_PRVWD:
-    if (record->event.pressed) {
-      if (keymap_config.swap_lctl_lgui) {
-	register_mods(mod_config(MOD_LALT));
-	register_code(KC_LEFT);
-      } else {
-	register_mods(mod_config(MOD_LCTL));
-	register_code(KC_LEFT);
-      }
-    } else {
-      if (keymap_config.swap_lctl_lgui) {
-	unregister_mods(mod_config(MOD_LALT));
-	unregister_code(KC_LEFT);
-      } else {
-	unregister_mods(mod_config(MOD_LCTL));
-	unregister_code(KC_LEFT);
-      }
-    }
-    break;
-  case KC_NXTWD:
-    if (record->event.pressed) {
-      if (keymap_config.swap_lctl_lgui) {
-	register_mods(mod_config(MOD_LALT));
-	register_code(KC_RIGHT);
-      } else {
-	register_mods(mod_config(MOD_LCTL));
-	register_code(KC_RIGHT);
-      }
-    } else {
-      if (keymap_config.swap_lctl_lgui) {
-	unregister_mods(mod_config(MOD_LALT));
-	unregister_code(KC_RIGHT);
-      } else {
-	unregister_mods(mod_config(MOD_LCTL));
-	unregister_code(KC_RIGHT);
-      }
-    }
-    break;
-  case KC_LSTRT:
-    if (record->event.pressed) {
-      if (keymap_config.swap_lctl_lgui) {
-	/* CMD-arrow on Mac, but we have CTL and GUI swapped */
-	register_mods(mod_config(MOD_LCTL));
-	register_code(KC_LEFT);
-      } else {
-	register_code(KC_HOME);
-      }
-    } else {
-      if (keymap_config.swap_lctl_lgui) {
-	unregister_mods(mod_config(MOD_LCTL));
-	unregister_code(KC_LEFT);
-      } else {
-	unregister_code(KC_HOME);
-      }
-    }
-    break;
-  case KC_LEND:
-    if (record->event.pressed) {
-      if (keymap_config.swap_lctl_lgui) {
-	/* CMD-arrow on Mac, but we have CTL and GUI swapped */
-	register_mods(mod_config(MOD_LCTL));
-	register_code(KC_RIGHT);
-      } else {
-	register_code(KC_END);
-      }
-    } else {
-      if (keymap_config.swap_lctl_lgui) {
-	unregister_mods(mod_config(MOD_LCTL));
-	unregister_code(KC_RIGHT);
-      } else {
-	unregister_code(KC_END);
-      }
-    }
-    break;
-  case KC_DLINE:
-    if (record->event.pressed) {
-      register_mods(mod_config(MOD_LCTL));
-      register_code(KC_BSPC);
-    } else {
-      unregister_mods(mod_config(MOD_LCTL));
-      unregister_code(KC_BSPC);
-    }
-    break;
-  case KC_COPY:
-    if (record->event.pressed) {
-      register_mods(mod_config(MOD_LCTL));
-      register_code(KC_C);
-    } else {
-      unregister_mods(mod_config(MOD_LCTL));
-      unregister_code(KC_C);
-    }
-    return false;
-  case KC_PASTE:
-    if (record->event.pressed) {
-      register_mods(mod_config(MOD_LCTL));
-      register_code(KC_V);
-    } else {
-      unregister_mods(mod_config(MOD_LCTL));
-      unregister_code(KC_V);
-    }
-    return false;
-  case KC_CUT:
-    if (record->event.pressed) {
-      register_mods(mod_config(MOD_LCTL));
-      register_code(KC_X);
-    } else {
-      unregister_mods(mod_config(MOD_LCTL));
-      unregister_code(KC_X);
-    }
-    return false;
-    break;
-  case KC_UNDO:
-    if (record->event.pressed) {
-      register_mods(mod_config(MOD_LCTL));
-      register_code(KC_Z);
-    } else {
-      unregister_mods(mod_config(MOD_LCTL));
-      unregister_code(KC_Z);
-    }
-    return false;
-
-    /* Smart Backspace Delete */
-
   case KC_RSFT:
   case KC_LSFT:
     shift_held = record->event.pressed;
     held_shift = keycode;
     break;
-  case KC_BSPC_DEL:
+  case KC_BSD:
     if (record->event.pressed) {
       if (shift_held) {
 	unregister_code(held_shift);
