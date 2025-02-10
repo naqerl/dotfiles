e90807b1efa5fdb06bd20a8acde155e0a11c864d
scipunch <scipunch@gmail.com>
2025-02-10 09:49:27 +0100

    add: Fetch issues and store in milestones

--- .emacs.d/scripts/linear.el
+++ .emacs.d/scripts/linear.el
@@ -37,7 +37,14 @@
       issues {
         nodes {
           id
+          identifier
           title
+          state {
+            name
+          }
+          projectMilestone {
+            name
+          }
         }
       }
     }
@@ -93,7 +100,7 @@ Returns a list of issues with their details."
     (linear--log-info "Fetched %s projects" (length projects))
     ;; Create directory if it doesn't exist
     (unless (file-exists-p linear-home-directory)
-      (linear--log-info "Creating linear home direcotry at %s"
+      (linear--log-info "Creating linear home directory at %s"
                         linear-home-directory)
       (make-directory linear-home-directory t))
 
@@ -110,9 +117,9 @@ Returns a list of issues with their details."
             (alist-get 'name project)
             (linear--fetch-project-issues (alist-get 'id project))))
        (error
-        (linear--log-info
-         "Error creating org file for project %s: %s"
-         (alist-get 'name project) (error-message-string err))))
+        (linear--log-info "Error during creating project %s: %s"
+                          (alist-get 'name project)
+                          (error-message-string err))))
 
      (linear--log-info "Sync completed. Processed %d projects."
                        created-files))))
@@ -122,27 +129,18 @@ Returns a list of issues with their details."
 PROJECT is an alist containing the Linear project data."
   (let* ((project-name (alist-get 'name project))
          (project-description (alist-get 'description project))
-         (project-directory-name (expand-file-name project-name))
-         ct
-         (project-issues-directory-name
-          (expand-file-name "issues" project-directory-name))
-         (project-index-file-name
-          (expand-file-name "index.org" project-directory-name)))
-
-    (unless (file-exists-p project-directory-name)
-      (make-directory project-directory-name t))
-
-    (unless (file-exists-p project-issues-directory-name)
-      (make-directory project-issues-directory-name t))
-
+         (default-directory
+          (linear--org-project-name-to-directory project-name))
+         (project-index-file-name "index.org"))
+    ;; Create project directory if not exists
+    (unless (file-exists-p default-directory)
+      (make-directory default-directory t))
     ;; Create org file with project details
-    (if (file-exists-p project-index-file-name)
-        nil
+    (unless (file-exists-p project-index-file-name)
       (with-temp-file project-index-file-name
         (insert (format "#+TITLE: %s\n" project-name))
         (insert (format "#+LINEAR_ID: %s\n" (alist-get 'id project)))
         (insert (format "#+STATE: %s\n\n" (alist-get 'state project)))
-
         ;; Add description if available
         (when project-description
           (insert "* Description\n")
@@ -151,10 +149,53 @@ PROJECT is an alist containing the Linear project data."
 
 (defun linear--org-create-issues (project-name issues)
   "Creates ISSUES for the PROJECT-NAME."
-  (cl-loop
-   for issue across issues do
-   (progn
-     (linear--log-info "Issue=%s" issue))))
+  (let ((default-directory
+         (expand-file-name
+          "issues"
+          (linear--org-project-name-to-directory project-name))))
+    (unless (file-exists-p default-directory)
+      (linear--log-info "Creating issues directory %s"
+                        default-directory)
+      (make-directory default-directory t))
+    (cl-loop
+     for issue across issues do
+     (progn
+       (linear--log-info "Issue=%s" issue)
+       (linear--org-create-issue issue)))))
+
+(defun linear--org-create-issue (issue)
+  "Cretes ISSUE org file."
+  (let* ((file-name
+          (concat
+           (alist-get 'identifier issue)
+           " "
+           (alist-get 'title issue)
+           ".org"))
+         (issue-milestone-name
+          (thread-last
+           issue
+           (alist-get 'projectMilestone) (alist-get 'name)))
+         (default-directory
+          (if issue-milestone-name
+              (expand-file-name issue-milestone-name)
+            default-directory)))
+    (unless (file-exists-p default-directory)
+      (linear--log-info "Creating milestone directory %s"
+                        default-directory)
+      (make-directory default-directory t))
+    (unless (file-exists-p file-name)
+      (with-temp-file file-name
+        (insert (format "#+TITLE: %s\n" (alist-get 'title issue)))
+        (insert (format "#+LINEAR_ID: %s\n" (alist-get 'id issue)))
+        (insert
+         (format "#+STATE: %s\n"
+                 (thread-last
+                  issue (alist-get 'state) (alist-get 'name))))
+        (insert (concat "\n* " (alist-get 'title issue)))))))
+
+(defun linear--org-project-name-to-directory (project-name)
+  "Returns absolute path to the project PROJECT-NAME directory."
+  (expand-file-name project-name linear-home-directory))
 
 ; end-region   -- Org Mode
 
