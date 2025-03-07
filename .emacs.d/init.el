29505ffeb4dd4ded2b62f221c4662c8ee3e81c2c
scipunch <scipunch@gmail.com>
2025-03-07 08:51:48 +0100

    add: Another emacs simplification

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,30 +1,5 @@
 (org-babel-load-file
  (expand-file-name "config.org" user-emacs-directory))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(all-the-icons-completion all-the-icons-dired better-jumper
-                              buffer-move compile-eslint
-                              consult-yasnippet csv-mode diminish
-                              dired-open dockerfile-mode dumb-jump eat
-                              elfeed elisp-autofmt elysium
-                              eshell-git-prompt free-keys gcmh
-                              git-gutter golden-ratio graphql
-                              graphql-mode helpful indent-guide
-                              load-env-vars magit make-project
-                              marginalia markdown-toc orderless
-                              org-auto-tangle pcmpl-args peep-dired
-                              plantuml-mode popper prettier-js pulsar
-                              request rust-docs rust-mode sqlformat
-                              sudo-edit toc-org vertico web-mode
-                              ws-butler yuck-mode))
- '(package-vc-selected-packages
-   '((make-project :url "https://github.com/scipunch/make-project")))
- '(warning-suppress-types '((comp))))
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (custom-set-faces
@@ -33,3 +8,31 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1"
+     "b49f66a2e1724db880692485a5d5bcb9baf28ed2a3a05c7a799fa091f24321da"
+     "833ddce3314a4e28411edf3c6efde468f6f2616fc31e17a62587d6a9255f4633"
+     "7fea145741b3ca719ae45e6533ad1f49b2a43bf199d9afaee5b6135fd9e6f9b8"
+     default))
+ '(package-selected-packages
+   '(all-the-icons-completion all-the-icons-dired better-jumper
+                              buffer-move compile-eslint csv-mode
+                              diminish dired-open dockerfile-mode
+                              dumb-jump eat elfeed elisp-autofmt
+                              elysium flimenu free-keys gcmh
+                              git-gutter golden-ratio graphql
+                              graphql-mode helpful load-env-vars magit
+                              make-project marginalia markdown-toc
+                              orderless org-auto-tangle peep-dired
+                              plantuml-mode prettier-js pulsar request
+                              rust-docs rust-mode solarized-theme
+                              sqlformat sudo-edit toc-org vertico
+                              web-mode yasnippet yuck-mode))
+ '(package-vc-selected-packages
+   '((make-project :url "https://github.com/scipunch/make-project")))
+ '(warning-suppress-types '((comp))))
