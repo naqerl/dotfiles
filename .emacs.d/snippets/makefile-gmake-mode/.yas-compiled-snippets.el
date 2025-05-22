4fb3c8fb1cb9eec9b3850915b54b794f4322de56
scipunch <scipunch@gmail.com>
2025-05-22 16:25:37 +0200

    feat: Add tsx-mode snippets and update others

--- .emacs.d/snippets/makefile-gmake-mode/.yas-compiled-snippets.el
+++ .emacs.d/snippets/makefile-gmake-mode/.yas-compiled-snippets.el
@@ -3,12 +3,12 @@
 ;;;
 (yas-define-snippets 'makefile-gmake-mode
 		     '(("deploy"
-			"deploy-$1: install-collections\n	ansible-playbook $1.yml deploy\n"
+			"deploy-$1: install-collections\n	ansible-playbook $1.yml\n"
 			"deploy" nil nil
-			((yas-indend-line 'fixed)
+			((yas-indent-line nil)
 			 (yas-wrap-around-region 'nil))
 			"/home/user/.emacs.d/snippets/makefile-gmake-mode/deploy"
 			nil nil)))
 
 
-;;; Do not edit! File generated at Thu May 22 14:46:02 2025
+;;; Do not edit! File generated at Thu May 22 16:23:52 2025
