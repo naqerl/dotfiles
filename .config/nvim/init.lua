4bea2ddc5b50f0eac509d117a349f4d8666fa531
scipunch <scipunch@gmail.com>
2025-07-05 15:07:10 +0200

    chore: Show cmd in mini statusline

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -204,15 +204,37 @@ require('lazy').setup({
       require('mini.surround').setup()
 
 			-- Simple and easy statusline.
-			--  You could remove this setup call if you don't like it,
-			--  and try some other statusline plugin
 			local statusline = require 'mini.statusline'
-			-- set use_icons to true if you have a Nerd Font
-			statusline.setup { use_icons = vim.g.have_nerd_font }
-			---@diagnostic disable-next-line: duplicate-set-field
-			statusline.section_location = function()
-				return '%2l:%-2v'
-			end
+			statusline.setup({
+				use_icons = vim.g.have_nerd_font,
+				content = {
+					active = function()
+						local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+						local git           = MiniStatusline.section_git({ trunc_width = 40 })
+						local diff          = MiniStatusline.section_diff({ trunc_width = 75 })
+						local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+						local lsp           = MiniStatusline.section_lsp({ trunc_width = 75 })
+						local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
+						local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+						local location      = '%2l:%-2v'
+						local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
+						local cmd						= require("noice").api.status.command.get()
+
+						return MiniStatusline.combine_groups({
+							{ hl = mode_hl,                  strings = { mode } },
+							{ hl = 'MiniStatuslineDevinfo',  strings = { git, diff, diagnostics, lsp } },
+							'%<', -- Mark general truncate point
+							{ hl = 'MiniStatuslineFilename', strings = { filename } },
+							'%=', -- End left alignment
+							{ strings = { cmd }},
+							'%=', -- End left alignment
+							{ hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+							{ hl = mode_hl,                  strings = { search, location } },
+						})
+					end,
+				},
+			})
+
 		end,
 	},
 	{ -- Highlight, edit, and navigate code
