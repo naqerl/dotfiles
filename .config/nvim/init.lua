24e068418995f7905a40ab3b2589c5c5a0c026fe
scipunch <scipunch@gmail.com>
2025-07-05 02:19:51 +0200

    chore: Use space as a leader

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,5 @@
-vim.g.mapleader = ';'
-vim.g.maplocalleader = ';'
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 vim.o.relativenumber = true
 vim.schedule(function()
 	vim.o.clipboard = 'unnamedplus'
