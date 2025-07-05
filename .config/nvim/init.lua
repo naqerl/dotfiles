5a30fe93a541bd823093db6c2054a763ae5e936a
scipunch <scipunch@gmail.com>
2025-07-05 03:12:03 +0200

    chore: Use notification alternative UI

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -344,6 +344,7 @@ require('lazy').setup({
 		dependencies = {
 			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
 		}
 	}
 })
