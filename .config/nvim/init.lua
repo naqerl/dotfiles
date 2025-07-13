24b0e81634825e1f12f4a1f19fede1b7214d1806
scipunch <scipunch@gmail.com>
2025-07-13 11:59:15 +0200

    chore: nvim improves

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -27,8 +27,8 @@ vim.o.expandtab = true
 -- Update buffer after file change
 vim.o.autoread = true
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
-  command = "if mode() != 'c' | checktime | endif",
-  pattern = "*",
+	command = "if mode() != 'c' | checktime | endif",
+	pattern = "*",
 })
 
 -- Terminal mode
@@ -73,56 +73,66 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
+	local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+	local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+	if vim.v.shell_error ~= 0 then
+		error('Error cloning lazy.nvim:\n' .. out)
+	end
 end
 
 ---@type vim.Option
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 require('lazy').setup({
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    event = 'VimEnter',
-    opts = {
-			auto_attach = true,
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-		keys = {
-			{ "<M-)>", "<cmd>lua require('gitsigns').nav_hunk('next')<cr>"},
-			{ "<M-(>", "<cmd>lua require('gitsigns').nav_hunk('prev')<cr>"},
-		}
-  },
-  { -- Fuzzy Finder (files, lsp, etc)
-    'nvim-telescope/telescope.nvim',
-    event = 'VimEnter',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      { -- If encountering errors, see telescope-fzf-native README for installation instructions
-        'nvim-telescope/telescope-fzf-native.nvim',
+	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
+		'lewis6991/gitsigns.nvim',
+		event = 'VimEnter',
+		config = function()
+			require('gitsigns').setup({
+				auto_attach = true,
+				signs = {
+					add = { text = '+' },
+					change = { text = '~' },
+					delete = { text = '_' },
+					topdelete = { text = '‾' },
+					changedelete = { text = '~' },
+				},
+			})
+			local gitsigns = require('gitsigns')
+			vim.keymap.set('n', "<M-)>", function()
+				gitsigns.nav_hunk('next')
+			end)
+			vim.keymap.set('n', "<M-(>", function()
+				gitsigns.nav_hunk('prev')
+			end)
+			vim.keymap.set('n', '<leader>hS', gitsigns.stage_buffer)
+			vim.keymap.set('n', '<leader>hR', gitsigns.reset_buffer)
+			vim.keymap.set('n', '<leader>hp', gitsigns.preview_hunk)
+			vim.keymap.set('n', '<leader>hi', gitsigns.preview_hunk_inline)
+			vim.keymap.set('n', '<leader>hd', gitsigns.diffthis)
+		end,
+	},
+	{ -- Fuzzy Finder (files, lsp, etc)
+		'nvim-telescope/telescope.nvim',
+		event = 'VimEnter',
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			{ -- If encountering errors, see telescope-fzf-native README for installation instructions
+				'nvim-telescope/telescope-fzf-native.nvim',
 
-        -- `build` is used to run some command when the plugin is installed/updated.
-        -- This is only run then, not every time Neovim starts up.
-        build = 'make',
+				-- `build` is used to run some command when the plugin is installed/updated.
+				-- This is only run then, not every time Neovim starts up.
+				build = 'make',
 
-        -- `cond` is a condition used to determine whether this plugin should be
-        -- installed and loaded.
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
-      },
-      { 'nvim-telescope/telescope-ui-select.nvim' },
-    },
-    config = function()
+				-- `cond` is a condition used to determine whether this plugin should be
+				-- installed and loaded.
+				cond = function()
+					return vim.fn.executable 'make' == 1
+				end,
+			},
+			{ 'nvim-telescope/telescope-ui-select.nvim' },
+		},
+		config = function()
 			require('telescope').setup {
 				extensions = {
 					['ui-select'] = {
@@ -151,58 +161,58 @@ require('lazy').setup({
 					},
 				},
 			}
-      pcall(require('telescope').load_extension, 'fzf')
-      pcall(require('telescope').load_extension, 'ui-select')
+			pcall(require('telescope').load_extension, 'fzf')
+			pcall(require('telescope').load_extension, 'ui-select')
 
-      local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>f', builtin.find_files)
-      vim.keymap.set('n', '<leader>g', builtin.live_grep)
-      vim.keymap.set('n', '<leader>b', builtin.buffers)
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags)
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps)
-      vim.keymap.set('n', '<leader>ss', builtin.builtin)
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string)
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics)
-      vim.keymap.set('n', '<leader>sr', builtin.resume)
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles)
-      vim.keymap.set('n', '<leader>r', builtin.registers)
+			local builtin = require 'telescope.builtin'
+			vim.keymap.set('n', '<leader>f', builtin.find_files)
+			vim.keymap.set('n', '<leader>g', builtin.live_grep)
+			vim.keymap.set('n', '<leader>b', builtin.buffers)
+			vim.keymap.set('n', '<leader>sh', builtin.help_tags)
+			vim.keymap.set('n', '<leader>sk', builtin.keymaps)
+			vim.keymap.set('n', '<leader>ss', builtin.builtin)
+			vim.keymap.set('n', '<leader>sw', builtin.grep_string)
+			vim.keymap.set('n', '<leader>sd', builtin.diagnostics)
+			vim.keymap.set('n', '<leader>sr', builtin.resume)
+			vim.keymap.set('n', '<leader>s.', builtin.oldfiles)
+			vim.keymap.set('n', '<leader>r', builtin.registers)
 
-      -- Slightly advanced example of overriding default behavior and theme
-      vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find)
+			-- Slightly advanced example of overriding default behavior and theme
+			vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find)
 
-      -- It's also possible to pass additional configuration options.
-      --  See `:help telescope.builtin.live_grep()` for information about particular keys
-      vim.keymap.set('n', '<leader>s/', function()
-        builtin.live_grep {
-          grep_open_files = true,
-          prompt_title = 'Live Grep in Open Files',
-        }
-      end, { desc = '[S]earch [/] in Open Files' })
+			-- It's also possible to pass additional configuration options.
+			--  See `:help telescope.builtin.live_grep()` for information about particular keys
+			vim.keymap.set('n', '<leader>s/', function()
+				builtin.live_grep {
+					grep_open_files = true,
+					prompt_title = 'Live Grep in Open Files',
+				}
+			end, { desc = '[S]earch [/] in Open Files' })
 
-      -- Shortcut for searching your Neovim configuration files
-      vim.keymap.set('n', '<leader>C', function()
-        builtin.find_files { cwd = vim.fn.stdpath 'config' }
-      end, { desc = '[S]earch [N]eovim files' })
-    end,
-  },
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-  { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
-    config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
+			-- Shortcut for searching your Neovim configuration files
+			vim.keymap.set('n', '<leader>C', function()
+				builtin.find_files { cwd = vim.fn.stdpath 'config' }
+			end, { desc = '[S]earch [N]eovim files' })
+		end,
+	},
+	{ 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+	{ -- Collection of various small independent plugins/modules
+		'echasnovski/mini.nvim',
+		config = function()
+			-- Better Around/Inside textobjects
+			--
+			-- Examples:
+			--  - va)  - [V]isually select [A]round [)]paren
+			--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+			--  - ci'  - [C]hange [I]nside [']quote
+			require('mini.ai').setup { n_lines = 500 }
 
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+			-- Add/delete/replace surroundings (brackets, quotes, etc.)
+			--
+			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+			-- - sd'   - [S]urround [D]elete [']quotes
+			-- - sr)'  - [S]urround [R]eplace [)] [']
+			require('mini.surround').setup()
 
 			-- Simple and easy statusline.
 			local statusline = require 'mini.statusline'
@@ -211,23 +221,23 @@ require('lazy').setup({
 				content = {
 					active = function()
 						local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
-						local git           = MiniStatusline.section_git({ trunc_width = 40 })
-						local diff          = MiniStatusline.section_diff({ trunc_width = 75 })
-						local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
-						local lsp           = MiniStatusline.section_lsp({ trunc_width = 75 })
-						local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
-						local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
-						local location      = '%2l:%-2v'
-						local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
+						local git	 = MiniStatusline.section_git({ trunc_width = 40 })
+						local diff	= MiniStatusline.section_diff({ trunc_width = 75 })
+						local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+						local lsp	 = MiniStatusline.section_lsp({ trunc_width = 75 })
+						local filename	= MiniStatusline.section_filename({ trunc_width = 140 })
+						local fileinfo	= MiniStatusline.section_fileinfo({ trunc_width = 120 })
+						local location	= '%2l:%-2v'
+						local search		= MiniStatusline.section_searchcount({ trunc_width = 75 })
 
 						return MiniStatusline.combine_groups({
-							{ hl = mode_hl,                  strings = { mode } },
-							{ hl = 'MiniStatuslineDevinfo',  strings = { git, diff, diagnostics, lsp } },
+							{ hl = mode_hl,	strings = { mode } },
+							{ hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics, lsp } },
 							'%<', -- Mark general truncate point
 							{ hl = 'MiniStatuslineFilename', strings = { filename } },
 							'%=', -- End left alignment
 							{ hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
-							{ hl = mode_hl,                  strings = { search, location } },
+							{ hl = mode_hl,	strings = { search, location } },
 						})
 					end,
 				},
@@ -311,20 +321,20 @@ require('lazy').setup({
 	},
 	{ "powerman/vim-plugin-ruscmd" },
 	{
-    "fredrikaverpil/godoc.nvim",
-    version = "*",
-    dependencies = {
-        { "nvim-telescope/telescope.nvim" }, -- optional
-        {
-            "nvim-treesitter/nvim-treesitter",
-            opts = {
-              ensure_installed = { "go" },
-            },
-        },
-    },
-    build = "go install github.com/lotusirous/gostdsym/stdsym@latest", -- optional
-    cmd = { "GoDoc" }, -- optional
-    opts = {
+		"fredrikaverpil/godoc.nvim",
+		version = "*",
+		dependencies = {
+			{ "nvim-telescope/telescope.nvim" }, -- optional
+			{
+				"nvim-treesitter/nvim-treesitter",
+				opts = {
+					ensure_installed = { "go" },
+				},
+			},
+		},
+		build = "go install github.com/lotusirous/gostdsym/stdsym@latest", -- optional
+		cmd = { "GoDoc" }, -- optional
+		opts = {
 			picker = {
 				type = "telescope"
 			}
@@ -342,7 +352,10 @@ require('lazy').setup({
 				},
 				messages = {
 					enabled = false,
-				}
+				},
+				presets = {
+					bottom_search = true,
+				},
 			})
 
 			-- Change cmdline highlighting
@@ -356,7 +369,13 @@ require('lazy').setup({
 		dependencies = {
 			"MunifTanjim/nui.nvim",
 		}
-	}
+	},
+	{
+		'nmac427/guess-indent.nvim',
+		config = function()
+			require('guess-indent').setup({})
+		end,
+	},
 })
 
 if vim.g.neovide then
@@ -377,8 +396,8 @@ end
 -- :make autocompletion
 vim.cmd([[
 	function! MakeCompletion(A,L,P) abort
-			let l:targets = systemlist('make -qp | awk -F'':'' ''/^[a-zA-Z0-9][^$#\/\t=]*:([^=]|$)/ {split($1,A,/ /);for(i in A)print A[i]}'' | grep -v Makefile | sort -u')
-			return filter(l:targets, 'v:val =~ "^' . a:A . '"')
+	let l:targets = systemlist('make -qp | awk -F'':'' ''/^[a-zA-Z0-9][^$#\/\t=]*:([^=]|$)/ {split($1,A,/ /);for(i in A)print A[i]}'' | grep -v Makefile | sort -u')
+	return filter(l:targets, 'v:val =~ "^' . a:A . '"')
 	endfunction
 	command! -nargs=* -complete=customlist,MakeCompletion Make !make <args>
 ]])
