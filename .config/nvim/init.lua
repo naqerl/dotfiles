f16e309c5ccbb7d9de5caa75f4bb154b55b4325a
scipunch <scipunch@gmail.com>
2025-07-26 11:25:54 +0200

    nvim: custom text objects

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -196,20 +196,22 @@ require('lazy').setup({
 	{ 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 	{ -- Collection of various small independent plugins/modules
 		'echasnovski/mini.nvim',
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects"
+		},
 		config = function()
-			-- Better Around/Inside textobjects
-			--
-			-- Examples:
-			--  - va)  - [V]isually select [A]round [)]paren
-			--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-			--  - ci'  - [C]hange [I]nside [']quote
-			require('mini.ai').setup { n_lines = 500 }
+			local spec_treesitter = require('mini.ai').gen_spec.treesitter
+			require('mini.ai').setup {
+				n_lines = 500,
+				custom_textobjects = {
+					F = spec_treesitter({ a = '@function.outer', i = '@function.inner' }),
+					L = spec_treesitter({
+						a = { '@conditional.outer', '@loop.outer' },
+						i = { '@conditional.inner', '@loop.inner' },
+					})
+				}
+			}
 
-			-- Add/delete/replace surroundings (brackets, quotes, etc.)
-			--
-			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-			-- - sd'   - [S]urround [D]elete [']quotes
-			-- - sr)'  - [S]urround [R]eplace [)] [']
 			require('mini.surround').setup()
 
 			require('mini.pairs').setup()
