253d3f854983e38d45f977510223c0e076170e41
naqerl <scipunch@gmail.com>
2025-12-28 11:49:30 +0100

    emacs: add jtsx and mermaid-mode packages

--- .emacs.d/.emacs.custom.el
+++ .emacs.d/.emacs.custom.el
@@ -6,9 +6,9 @@
  '(package-selected-packages
    '(change-inner clipetty consult diminish doom-themes dumb-jump eat
                   embark f git-gutter go-mode golden-ratio jq-mode
-                  magit markdown-mode orderless solidity-mode
-                  sudo-edit treesit-auto undo-tree vertico yaml-mode
-                  yasnippet)))
+                  jtsx magit markdown-mode mermaid-mode orderless
+                  solidity-mode sudo-edit treesit-auto undo-tree
+                  vertico yaml-mode yasnippet)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
